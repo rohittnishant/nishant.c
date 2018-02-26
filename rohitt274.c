787639076fd1788884da28575f63fdc61c858d9b
rohittnishant <36066907+rohittnishant@users.noreply.github.com>
2018-02-26 16:22:14 +0530

    Create rohitt274.c

---
+++ rohitt274.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include<conio.h>
+int main() 
+{
+	int n;
+	printf("enter the number:");
+	scanf("%d",&n);
+	if(n>=1 && n<=10)
+	{
+		printf("\n yes");
+	}
+	else
+	{
+		printf("\n no");
+	}
+  getch();
+	return 0;
+}
